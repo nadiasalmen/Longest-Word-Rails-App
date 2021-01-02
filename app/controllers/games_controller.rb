ba5d7e0be88b956636950b5e86cb50220466aff8
Nadia Salmen <nadiasalmen@gmail.com>
2021-01-02 12:27:53 -0300

    Implement the GamesController#score logic.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,35 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = []
-    10.times do
-      @letters << ('A'..'Z').to_a.sample
-    end
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? do |letter|
+      # grid.include?(letter) => only checks if the letter is included
+      word.count(letter) <= letters.count(letter)
+    end
+  end
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    word_serialized = open(url).read
+    english_word = JSON.parse(word_serialized)
+    english_word['found']
   end
 end
